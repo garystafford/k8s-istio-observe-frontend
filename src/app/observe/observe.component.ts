c6111d7df6264e13392ebe48f66278a6a766435a
Gary A. Stafford <garystafford@rochester.rr.com>
2019-03-13 19:57:16 -0400

    Refactor Boostrap UI

--- src/app/observe/observe.component.ts
+++ src/app/observe/observe.component.ts
@@ -18,20 +18,25 @@ export class ObserveComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.apiURL = 'localhost';
+    this.apiURL = 'api.dev.example-api.com';
   }
 
   changeApiUrl() {
     this.apiURLFull = 'http://' + this.apiURL + '/api/ping';
     this._logger.info('apiURLFull:', this.apiURLFull);
 
+    const t0 = performance.now();
     this._service.getTraces(this.apiURLFull).subscribe(
       data => {
         this.traces = data;
-        this._logger.info('traces:', this.traces);
+        this._logger.debug('traces:', this.traces);
       },
-      err => this._logger.error(err),
-      () => this._logger.info('Done loading traces')
+      err => this._logger.error(err.message),
+      () => {
+        const t1 = performance.now();
+        return this._logger.info('Call to getTraces took ' + (t1 - t0).toFixed(2) + ' ms');
+      }
     );
+
   }
 }
